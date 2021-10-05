414ab76a827d554b19af7d596e65db9664786594
bughou <bughou@gmail.com>
2021-10-05 19:44:45 +0800

    save

--- pool.go
+++ pool.go
@@ -2,8 +2,6 @@ package pool
 
 import (
 	"io"
-	"net/url"
-	"strconv"
 	"sync"
 	"time"
 )
@@ -14,58 +12,41 @@ type Value struct {
 }
 
 type Pool struct {
-	mu sync.Mutex
-
-	// max number of resouces can be opened at a given moment.
-	maxOpen int // <= 0 means unlimited
-	// current number of opened resources, including the idle ones and the busy ones.
-	numOpen int
-	// the resource open func
+	// The resource open func
 	openFunc func() (io.Closer, error)
-
-	// max number of idle resources to keep. < 0 means unlimited.
+	// Max number of resources can be opened at a given moment. Default value 10 is used if maxOpen <= 0.
+	maxOpen int
+	// Max number of idle resources to keep. Value of 0 is used if maxIdle < 0.
 	maxIdle int
-	// the idle resources map.
-	idle         map[uint64]Value
-	idleStartKey uint64
-	// how long after a resource keep idle, it will be released. <=0 means a resouece won't be released for idle time reason.
-	idleTimeout time.Duration
+	// Close a resource after how long it's opened. maxLine <= 0 means never close for this reason.
+	maxLife time.Duration
 
-	// notifcation when a resource becomes idle or released.
-	notification chan struct{}
+	// internal fields
+	mu sync.Mutex
+	// current number of opened resources, including the idle ones and the busy ones.
+	numOpen   int
+	resources chan interface{}
 }
 
-func New(open func() (io.Closer, error), q url.Values) (*Pool, error) {
-	maxOpen, maxIdle, idleTimeout, err := parseParams(q)
-	if err != nil {
-		return nil, err
+func New(
+	openFunc func() (io.Closer, error), maxOpen, maxIdle int, maxLife time.Duration,
+) *Pool {
+	if maxOpen <= 0 {
+		maxOpen = 10
+	}
+	if maxIdle < 0 {
+		maxIdle = 0
+	}
+	if maxIdle > maxOpen {
+		maxIdle = maxOpen
 	}
 
 	p := &Pool{
-		maxOpen: maxOpen, openFunc: open,
-		maxIdle: maxIdle, idle: make(map[uint64]Value), idleTimeout: idleTimeout,
-		notification: make(chan struct{}),
+		openFunc:  openFunc,
+		maxOpen:   maxOpen,
+		maxIdle:   maxIdle,
+		maxLife:   maxLife,
+		resources: make(chan interface{}, maxIdle),
 	}
-	return p, nil
-}
-
-func parseParams(q url.Values) (maxOpen, maxIdle int, idleTimeout time.Duration, err error) {
-	if str := q.Get(`maxOpen`); str != `` {
-		if maxOpen, err = strconv.Atoi(str); err != nil {
-			return
-		}
-	}
-	if str := q.Get(`maxIdle`); str != `` {
-		if maxIdle, err = strconv.Atoi(str); err != nil {
-			return
-		}
-	} else {
-		maxIdle = -1
-	}
-	if str := q.Get(`idleTimeout`); str != `` {
-		if idleTimeout, err = time.ParseDuration(str); err != nil {
-			return
-		}
-	}
-	return
+	return p
 }
